9c379b8702e54f2b5632ccd6db0b6a17a208006c
陈超 <247135449@qq.com>
2017-07-08 20:35:00 +0800

    1

---
+++ costFunction.m
@@ -0,0 +1,1 @@
+function [jVal, gradient] = costFunction(theta)jVal = (theta(1) - 5)^2 + (theta(2) - 5)^2;gradient = zeros(2, 1);gradient(1) = 2*(theta(1)-5);gradient(2) = 2*(theta(2)-5);  % 调用costFunction:% options = optimset('GradObj', 'on', 'MaxIter', '100');% initalTheta = zeros(2, 1);% [optTheta, functionVal, exitFlag] = fminunc(@costFunction, initalTheta, options);
